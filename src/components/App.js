cee113dfc33bcf508a9adfa1088204e6b74c82b2
CYCHA-GH <aksemf1140@naver.com>
2022-04-06 20:15:45 +0900

    App.js minor

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
+import {useState} from "react";
+import AppRouter from "./Router";
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div>
-     APP
-    </div>
+    <>
+    <AppRouter isLoggedIn={isLoggedIn}/>
+    <footer>&copy; {new Date().getFullYear()} Nwitter^^ </footer>
+    </>
   );
 }
 
