fb557e78fe57ebb5cb8b177756a80d7a6275d27f
CYCHA-GH <aksemf1140@naver.com>
2022-04-06 19:41:19 +0900

    Router.js, README 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,21 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {useState} from "react";
+import { HashRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import Auth from "../routes/Auth"
+import Home from "../routes/Home";
+import Auth from "../routes/Home"
 
 const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return(
         <Router>
             <Switch>
-                <Route/>
+                {isLoggedIn ? (
+                    <Routes exact path="/">
+                    <Home/>
+                    </Routes>
+                ) : (0
+
+                )}
             </Switch>
         </Router>
     )
