f1fc106eca69793a91424b321869cf1055d7b78d
CYCHA-GH <aksemf1140@naver.com>
2022-04-15 21:53:47 +0900

    merge commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,20 @@
-import {useState} from "react";
-import { HashRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({isLoggedIn}) => {
     return(
         <Router>
             <Switch>
                 {isLoggedIn ? (
-                    <Routes exact path="/">
+                    <Route exact path="/">
                     <Home/>
-                    </Routes>
+                    </Route>
                 ) : (
-                    <Routes exact path="/">
+                    <Route exact path="/">
                     <Auth/>
-                    </Routes>
+                    </Route>
+
                 )}
             </Switch>
         </Router>
