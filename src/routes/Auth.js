bb9489ba7f1255b677c00533f367aed89c78e943
CYCHA-GH <aksemf1140@naver.com>
2022-04-27 20:50:19 +0900

    비교를 위한 파일내용 추가, 우선 커밋

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,57 +1,146 @@
+import { authService, firebaseInstance } from "fbase";
 import { useState } from "react";
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount] = useState(true);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
   
-  const onChange = (event) => {
-    const {
-      target : {name, value},
-    } = event;
-    if(name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
+    const onChange = (event) => {
+      const {
+        target: { name, value },
+      } = event;
+      if (name === "email") {
+        setEmail(value);
+      } else if (name === "password") {
+        setPassword(value);
+      }
+    };
+  
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        let data;
+        if (newAccount) {
+          data = await authService.createUserWithEmailAndPassword(
+            email,
+            password
+          );
+        } else {
+          data = await authService.signInWithEmailAndPassword(email, password);
+        }
+        console.log(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+  
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+  
+    const onSocialClick = async (event) => {
+      const {
+        target: { name },
+      } = event;
+      let provider;
+      if (name === "google") {
+        provider = new firebaseInstance.auth.GoogleAuthProvider();
+      } else if (name === "github") {
+        provider = new firebaseInstance.auth.GithubAuthProvider();
+      }
+      const data = await authService.signInWithPopup(provider);
+      console.log(data);
+    };
+  
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            name="email"
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={onChange}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={onChange}
+          />
+          <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+          {error}
+        </form>
+        <span onClick={toggleAccount}>
+          {newAccount ? "Sign In" : "Create Account"}
+        </span>
+        <div>
+          <button onClick={onSocialClick} name="google">
+            Continue with Google
+          </button>
+          <button onClick={onSocialClick} name="github">
+            Continue with Github
+          </button>
+        </div>
+      </div>
+    );
   };
+  
+  export default Auth;
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    if (newAccount) {
-    } else {
-    }
-  };
-  return (
-    <div>
-      <form onSubmit={ onSubmit }>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={ onChange }
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input type="submit" placeholder="Log In" value={newAccount ? "Create Account" : "Log In"} required />
-      </form>
-      <div className="authBtns">
-                <button  name="google" className="authBtn">
-                    Continue with Google
-                </button>
-                <button  name="github" className="authBtn">
-                    Continue with Github
-                </button>
-            </div>
-    </div>
-  );
-}
+// const Auth = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [newAccount] = useState(true);
+//   const toggleAccount = () => setNewAccount((prev)=>!prev);
+//   const onChange = (event) => {
+//     const {
+//       target : {name, value},
+//     } = event;
+//     if(name === "email") {
+//       setEmail(value);
+//     } else if (name === "password") {
+//       setPassword(value);
+//     }
+//   };
 
-export default Auth
+//   const onSubmit = async (event) => {
+//     event.preventDefault();
+//     if (newAccount) {
+//     } else {
+//     }
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={ onSubmit }>
+//         <input
+//           name="email"
+//           type="email"
+//           placeholder="Email"
+//           required
+//           value={email}
+//           onChange={ onChange }
+//         />
+//         <input
+//           name="password"
+//           type="password"
+//           placeholder="Password"
+//           required
+//           value={password}
+//           onChange={onChange}
+//         />
+//         <input type="submit" placeholder="Log In" value={newAccount ? "Create Account" : "Log In"} required />
+//       </form>
+//       <span onClick={toggleAccount}>
+//             {newAccount ? "Sign In" : "Create Account"}
+//       </span>
+//       <div className="authBtns">
+//                 <button  name="google" className="authBtn"> Continue with Google </button>
+//                 <button  name="github" className="authBtn"> Continue with Github </button>
+//             </div>
+//     </div>
+//   );
+// }
+// export default Auth;
