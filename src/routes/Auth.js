43165fe6af29f94d43033610bd3f9dc2a0964d5b
CYCHA-GH <58205815+CYCHA-GH@users.noreply.github.com>
2022-05-04 19:54:59 +0900

    !!clonepush

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,74 +1,63 @@
-import { authService, firebaseInstance } from "firebase";
-import { useState } from "react";
+import { useState } from "react"
+import { authService } from "fbase"
+
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
-  const toggleAccount = () => setNewAccount((prev)=>!prev);
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [newAccount, setNewAccount] = useState("true")
+  const [error, setError] = useState("")
+
   const onChange = (event) => {
     const {
-      target : {name, value},
-    } = event;
+      target: { name, value },
+    } = event
     if(name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
+      setEmail(value)
+    } else if(name === "password") {
+      setPassword(value)
     }
-  };
-
-   const onSocialClick = async (event) => {
-    const {
-      target: { name },
-    } = event;
-    let provider;
-    if (name === "google") {
-      provider = new firebaseInstance.auth.GoogleAuthProvider();
-    } else if (name === "github") {
-      provider = new firebaseInstance.auth.GithubAuthProvider();
-    }
-    const data = await authService.signInWithPopup(provider);
-    console.log(data);
-  };
+  }
 
   const onSubmit = async (event) => {
-    event.preventDefault();
-    if (newAccount) {
-    } else {
+    event.preventDefault()
+    try {
+      let data
+      if(newAccount) {
+        // create newAccount
+        data = await authService.createUserWithEmailAndPassword(email, password)
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password)
+      }
+      console.log(data);
+    } catch(error) {
+      setError(error.message)
     }
-  };
+  }
+
+  const toggleAccount = () => setNewAccount((prev) => !prev)
+
+  const onSocialClick = (e) => {
+    console.log(e.target.name)
+  }
+
   return (
     <div>
-      <form onSubmit={ onSubmit }>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={ onChange }
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input type="submit" placeholder="Log In" value={newAccount ? "Create Account" : "Log In"} required />
+      <form onSubmit={onSubmit}>
+        <input name="email" type="email" placeholder="이메일을 입력하세요" required value={email} onChange={onChange} />
+        <input name="password" type="password" placeholder="비밀번호를 입력하세요" required value={password} onChange={onChange} />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+        {error}
       </form>
       <span onClick={toggleAccount}>
-            {newAccount ? "Sign In" : "Create Account"}
+        {newAccount ? "Sign In" : "Create Account"}
       </span>
       <div>
-      <button onClick={onSocialClick} name="google">
-          Continue with Google
-        </button>
-        <button onClick={onSocialClick} name="github">
-          Continue with Github
-        </button>
-            </div>
+        <button onClick={onSocialClick} name="google"> Continue with Google</button>
+        <button onClick={onSocialClick} name="github"> Continue with GitHub</button>
+      </div>
     </div>
-  );
+  )
 }
-export default Auth;
+
+export default Auth
