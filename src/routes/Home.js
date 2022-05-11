0beb26a0021248244d947736b8908de106ab1993
CYCHA-GH <58205815+CYCHA-GH@users.noreply.github.com>
2022-05-11 21:33:16 +0900

    5/11

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,9 +3,11 @@ import{useState} from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState("");
 
     const getNweets = async () => {
         const dbNweets = await dbService.collection("nweets").get();
+        // dbNweets.forEach(document) =>
         console.log(dbNweets);
     }
 
